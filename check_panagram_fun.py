396424a530863d877e3481953b9d4237a11508a8
tarun-agarwal-25 <95695868+tarun-agarwal-25@users.noreply.github.com>
2022-05-29 23:21:57 +0530

    Create check_panagram_fun.py

---
+++ check_panagram_fun.py
@@ -0,0 +1,10 @@
+def isPangram(s):
+    l=list(s)
+    x=set(l)
+    if len(x)==26:
+        print("It is Pangram")
+    else:
+        print("It is not Pangram")
+
+st=input("Enter a string: ")
+isPangram(st)
